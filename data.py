ec804c231747ded97d8d31056ef3a80fa1e910ba
jaehwan1119 <jaehwankim1119@gmail.com>
2024-06-14 02:22:50 +0900

    add data file

---
+++ data.py
@@ -0,0 +1,65 @@
+from typing import List, Dict, Any
+
+# 임의의 데이터
+document_data = {
+    'id' : "EXAU2302308290",
+    'metadata' : {
+        "title": "국립국어원 웹 말뭉치 추출 EXAU2302308290",
+        "creator": "국립국어원",
+        "distributor": "국립국어원",
+        "year": "2023",
+        "category": "웹 > 리뷰 > 누리소통망",
+        "annotation_level": "부적절 발언 탐지",
+        "sampling": "본문 전체 / 부분 추출 - 임의 추출 / 부분 추출 - 특정 부분 추출"
+    },
+    'document' : {},
+}
+
+# document data
+class Data:
+    def __init__(self, id: str = None, metadata: Dict[str, Any] = None, paragraph: List[dict] = None,
+                 sentence: List[dict] = None, immoral_expression: List[dict] = None):
+        self.id = id # json으로부터 불러옴
+        self.metadata = metadata # json으로부터 불러옴
+        self.paragraph = paragraph
+        self.sentence = sentence
+        self.immoral_expression = immoral_expression
+
+# data의 원소 (json으로부터 불러옴)
+class Paragraph:
+    def __init__(self, id: str = None, form: str = None, original_form: str = None,):
+        self.id = id
+        self.form = form
+        self.original_form = original_form
+
+# data의 원소 (excel로부터 불러옴)
+class Sentence:
+    def __init__(self, id: str = None, form: str = None, original_form: str = None):
+        self.id = id
+        self.form = form
+        self.original_form = original_form
+
+# data의 원소 (excel로부터 불러옴)
+class ImmoralExpression:
+    def __init__(self, expression_id: str = None, expression_form: str = None,
+                 expression: dict = None):
+        self.expression_id = expression_id
+        self.expression_form = expression_form
+        self.expression = expression
+
+# immoral_expression의 원소(excel로부터 불러옴)
+class Expression:
+    def __init__(self, explicitness: List[dict] = None, sentiment: str = None,
+                 domains: str = None, intensity: int = None):
+        self.explicitness = explicitness
+        self.sentiment = sentiment
+        self.domains = domains
+        self.intensity = intensity
+
+# expression의 원소(excel로부터 불러옴)
+class Explicitness:
+    def __init__(self, type: str = None, form: str = None, begin: int = None, end: int = None):
+        self.type = type
+        self.form = form
+        self.begin = begin
+        self.end = end
--- excel2json.py
+++ excel2json.py
@@ -1,32 +1,29 @@
 import re
-
+import numpy as np
 import pandas as pd
 from glob import glob
 import os
 import json
 
+from data import *
+
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-# document 정보(임의로 설정)
-document_id = "EXAU2302308290"
-document_metadata = {
-    "title": "국립국어원 웹 말뭉치 추출 EXAU2302308290",
-    "creator": "국립국어원",
-    "distributor": "국립국어원",
-    "year": "2023",
-    "category": "웹 > 리뷰 > 누리소통망",
-    "annotation_level": "부적절 발언 탐지",
-    "sampling": "본문 전체 / 부분 추출 - 임의 추출 / 부분 추출 - 특정 부분 추출"
-}
-
-
-
-# excel 데이터를 쌓을 document 리스트
-document = []
-
 # excel, json 파일 path
-excel_path = '/Users/dataly/Desktop/E/result_excel.xlsx'
+# excel_path = '/Users/dataly/Desktop/E/result_excel.xlsx'
+excel_path = '/Users/gimjaehwan/Desktop/E/result_excel.xlsx'
+
+# excel파일의 DataFrame
+excel_df = pd.read_excel(excel_path, dtype=str)
+
+# excel에서 데이터를 읽어올 때 빈 칸의 nan을 ''로 교체
+excel_df = excel_df.replace(np.nan, '', regex=True)
+new_excel_dict = excel_df.to_dict(orient='dict')
+
+# json 형태로 출력
+json_data = json.dumps(new_excel_dict, indent=4, ensure_ascii=False)
+print(json_data)
 
 # excel 파일 읽어오기
 df = pd.read_excel(excel_path, engine='openpyxl')
@@ -36,17 +33,18 @@ excel_id = df['id'][0]
 excel_form = df['output'][0]
 
 # excel의 id로부터 파일명을 추출
-json_file_path = '/Users/dataly/Desktop/ABU/' + excel_id.split('.')[0] + '.json'
+# json_file_path = '/Users/dataly/Desktop/ABU/' + excel_id.split('.')[0] + '.json'
+json_file_path = '/Users/gimjaehwan/Desktop/ABU/' + excel_id.split('.')[0] + '.json'
 
 # json 파일 열기
 f = open(json_file_path)
 data = json.load(f)
 json_df = pd.json_normalize(data, record_path=['document'])
 
-print(json_df)
+# print(json_df)
 # print(excel_form)
 
 # 불러온 json DataFrame에서 excel의 id에 해당하는 부분 추출
 
 
-# print(df)
+# for file in txt_file:
