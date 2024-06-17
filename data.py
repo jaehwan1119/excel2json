49b967679f2a75ba9cd25388f960169635cd851e
jaehwan1119 <jaehwankim1119@gmail.com>
2024-06-17 12:04:33 +0900

    make json form

--- data.py
+++ data.py
@@ -12,18 +12,26 @@ document_data = {
         "annotation_level": "부적절 발언 탐지",
         "sampling": "본문 전체 / 부분 추출 - 임의 추출 / 부분 추출 - 특정 부분 추출"
     },
-    'document' : {},
+    'document': []
 }
 
 # document data
 class Data:
-    def __init__(self, id: str = None, metadata: Dict[str, Any] = None, paragraph: List[dict] = None,
-                 sentence: List[dict] = None, immoral_expression: List[dict] = None):
-        self.id = id # json으로부터 불러옴
-        self.metadata = metadata # json으로부터 불러옴
-        self.paragraph = paragraph # json으로부터 불러옴
-        self.sentence = sentence # excel로부터 불러옴
-        self.immoral_expression = immoral_expression # excel로부터 불러옴
+    def __init__(self):
+        self.id = '' # json으로부터 불러옴
+        self.metadata = {} # json으로부터 불러옴
+        self.paragraph = [] # json으로부터 불러옴
+        self.sentence = [] # excel로부터 불러옴
+        self.immoral_expression = [] # excel로부터 불러옴
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "metadata": self.metadata,
+            "paragraph": self.paragraph,
+            "sentence": self.sentence,
+            "immoral_expression": self.immoral_expression
+        }
 
 # data의 원소 (json으로부터 불러옴)
 class Paragraph:
@@ -39,6 +47,16 @@ class Sentence:
         self.form = form
         self.original_form = original_form
 
+    def __str__(self):
+        return (f"Sentence(id={self.id}, form={self.form}, original_form={self.original_form})")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "form": self.form,
+            "original_form": self.original_form
+        }
+
 # data의 원소 (excel로부터 불러옴)
 class ImmoralExpression:
     def __init__(self, expression_id: str = None, expression_form: str = None,
@@ -46,6 +64,16 @@ class ImmoralExpression:
         self.expression_id = expression_id
         self.expression_form = expression_form
         self.expression = expression
+
+    def __str__(self):
+        return (f"ImmoralExpression(id={self.expression_id}, form={self.expression_form}, expression={self.expression})")
+
+    def to_dict(self):
+        return {
+            "expression_id": self.expression_id,
+            "expression_form": self.expression_form,
+            "expression": self.expression
+        }
 
 # immoral_expression의 원소(excel로부터 불러옴)
 class Expression:
@@ -56,6 +84,17 @@ class Expression:
         self.domains = domains
         self.intensity = intensity
 
+    def __str__(self):
+        return (f"Expression(explicitness={self.explicitness}, sentiment={self.sentiment}, domains={self.domains}, intensity={self.intensity})")
+
+    def to_dict(self):
+        return {
+            "explicitness": self.explicitness,
+            "sentiment": self.sentiment,
+            "domains": self.domains,
+            "intensity": self.intensity
+        }
+
 # expression의 원소(excel로부터 불러옴)
 class Explicitness:
     def __init__(self, type: str = None, form: str = None, begin: int = None, end: int = None):
@@ -63,3 +102,14 @@ class Explicitness:
         self.form = form
         self.begin = begin
         self.end = end
+
+    def __str__(self):
+        return (f"Explicitness(type={self.type}, form={self.form}, begin={self.begin}, end={self.end})")
+
+    def to_dict(self):
+        return {
+            "type": self.type,
+            "form": self.form,
+            "begin": self.begin,
+            "end": self.end
+        }
--- excel2json.py
+++ excel2json.py
@@ -39,7 +39,8 @@ def replace_expression(text: str) -> str:
 
     return result
 
-
+def find_keys(dictionary, value):
+    return [key for key, val in dictionary.items() if val == value]
 
 # excel, json 파일 path
 excel_path = '/Users/dataly/Desktop/E/result_excel.xlsx'
@@ -54,7 +55,7 @@ new_excel_dict = excel_df.to_dict(orient='dict')
 
 # json 형태로 출력
 json_data = json.dumps(new_excel_dict, indent=4, ensure_ascii=False)
-print(json_data)
+# print(json_data)
 
 dict_type = ['id', 'type', 'output', 'modify', 'diff_1', 'diff_2']
 temp_form = []
@@ -68,55 +69,90 @@ temp_dict = {
         'diff_2': []
     }
 
+docu_data = Data()
+sentence = Sentence()
 immoral_expression = ImmoralExpression()
 expression = Expression()
 explicitness = Explicitness()
 
+# sentence를 저장할 결과 list
+i=0
 # excel dictionary 전체순회
-for i in range(len(new_excel_dict['id'])):
+while i != len(new_excel_dict['id']):
     # excel dictionary의 요소를 임시 dictionary에 추가
     for dtype in dict_type:
         # form 추출
         if new_excel_dict[dtype][i] == 'form':
-            # form에서 id와 detail 추출
+            # form의 내용 (sentence에 추가할 데이터)
             temp_form = find_pattern(new_excel_dict['output'][i], 1)
+
+            # form의 내용에서 <~:expression> 제거
+            for j in range(len(temp_form)):
+                temp_form[j] = replace_expression(temp_form[j])
+
+            # form의 id (sentence에 추가할 데이터)
             temp_form_id = find_pattern(new_excel_dict['output'][i], 0)
+
+            # sentence의 id, form, form_origin 저장
+            for id, form in zip(temp_form_id, temp_form):
+                sentence.__init__(id, form, form)
+                docu_data.sentence.append(sentence.to_dict())
+
         temp_dict[dtype].append(new_excel_dict[dtype][i])
+    i += 1
+    # id 단위로 끊어서 temp_dict을 내보냄
+    if temp_dict['id'][0] != new_excel_dict['id'][i]:
+        # 데이터를 json에 추가할 수 있도록 정제하고 class에 저장 후 temp_dict, temp_form 초기화
+        k = 1
+        length = len(temp_dict['output'])
+        while k < length:
 
-    # 만약 현재 index의 new_excel_dict id가 저장중인 temp_dict의 id와 같지 않다면
-    # -> 데이터를 json에 추가할 수 있도록 정제하고 class에 저장 후 temp_dict, temp_form 초기화
-    if i == len(new_excel_dict['id']) - 1 or temp_dict['id'][0] != new_excel_dict['id'][i+1]:
-        # 데이터 정제
-        # form detail의 <~:expression> 제거
-        for j in range(len(temp_form)):
-            temp_form[j] = replace_expression(temp_form[j])
+            expression_id = find_pattern(temp_dict['output'][k], 0)[0]  # 선택문장 id
+            expression_form = replace_expression(find_pattern(temp_dict['output'][k], 1)[0])  # 선택문장 내용
+            k += 1
+            # 여기서부터
+            explicitness_list = []
+            exp_form_list = temp_dict['output'][k].split(', ')
+            k += 1
+            for explicitness_data in exp_form_list:
+                explicitness_form = explicitness_data  # 부정적 발언 list
+                explicitness_type = 'True' if temp_dict['output'][k] == '명시' else 'False'
+                explicitness_begin = expression_form.find(explicitness_form)
+                explicitness_end = explicitness_begin + len(explicitness_form)
 
-        # 데이터 저장
-        expression_id = find_pattern(temp_dict['output'][1], 0) # <- 선택문장 id
-        expression_form = replace_expression(find_pattern(temp_dict['output'][1], 1)[0]) # <- 선택문장 내용
-        for explicitness_data in temp_dict['output'][2]:
-            explicitness_form = temp_dict['output'][2][0] # 부정적 발언 list
-            explicitness_type = 'True' if temp_dict['output'][3] == '명시' else 'False'
-            explicitness_begin = expression_form.find(explicitness_form)
-            explicitness_end = explicitness_begin + len(explicitness_form)
+                explicitness.__init__(explicitness_type, explicitness_form, explicitness_begin, explicitness_end)
+                explicitness_list.append(explicitness.to_dict())
+            # 여기까지 explicitness 생성
 
-            explicitness.__init__(explicitness_type, explicitness_form, explicitness_begin, explicitness_end)
-        expression_sentiment = temp_dict['output'][4]
-        expression_intensity = temp_dict['output'][5]
-        expression_domains = temp_dict['output'][6]
+            # 여기서부터
+            k += 1
+            expression_sentiment = temp_dict['output'][k]  # 긍/부정 여부
+            k += 1
+            expression_intensity = temp_dict['output'][k]  # 강도
+            k += 1
+            expression_domains = temp_dict['output'][k]  # 영역
+            k += 1
 
+            expression.__init__(explicitness_list, expression_sentiment, expression_domains, expression_intensity)
+            immoral_expression.__init__(expression_id, expression_form, expression.to_dict())
+            docu_data.immoral_expression.append(immoral_expression.to_dict())
+            # 여기까지 expression 생성
 
+        # json 데이터 불러오기
+        json_id = temp_dict['id'][0]
+        json_file_path = '/Users/dataly/Desktop/ABU/' + json_id.split('.')[0] + '.json'
 
+        # # excel의 id에 대한 json 파일 불러오기
+        f = open(json_file_path)
+        data = json.load(f)
+        search_result = find_object_by_id(data['document'], json_id)
 
+        docu_data.id = search_result['id']
+        docu_data.metadata = search_result['metadata']
+        docu_data.paragraph = search_result['paragraph']
 
-
-        print('[temp_dict]')
-        print(temp_dict)
-        print('[temp_form]')
-        print(temp_form)
-        print('[temp_form_id]')
-        print(temp_form_id)
-        print('=' * 50)
+        json_data = json.dumps(docu_data.to_dict(), indent=4, ensure_ascii=False)
+        print(json_data)
 
         # 초기화
         temp_form = []
@@ -130,26 +166,70 @@ for i in range(len(new_excel_dict['id'])):
             'diff_2': []
         }
 
-
-# # 읽어온 excel 파일에서 id값 읽기 (현재는 맨 첫번째 아이디: ERRW1908000023.267)
-# excel_id = excel_df['id'][0]
-# excel_form = excel_df['output'][0]
+# # 데이터를 json에 추가할 수 있도록 정제하고 class에 저장 후 temp_dict, temp_form 초기화
+# k=1
+# length = len(temp_dict['output'])
+# while k < length:
 #
-# # excel의 id에 대한 json 파일명(ERRW1908000023.json) 생성
-# json_file_path = '/Users/dataly/Desktop/ABU/' + excel_id.split('.')[0] + '.json'
-# # json_file_path = '/Users/gimjaehwan/Desktop/ABU/' + excel_id.split('.')[0] + '.json'
+#     expression_id = find_pattern(temp_dict['output'][k], 0)[0]  # 선택문장 id
+#     expression_form = replace_expression(find_pattern(temp_dict['output'][k], 1)[0])  # 선택문장 내용
+#     k+=1
+#     # 여기서부터
+#     explicitness_list = []
+#     exp_form_list = temp_dict['output'][k].split(', ')
+#     k+=1
+#     for explicitness_data in exp_form_list:
+#         explicitness_form = explicitness_data # 부정적 발언 list
+#         explicitness_type = 'True' if temp_dict['output'][k] == '명시' else 'False'
+#         explicitness_begin = expression_form.find(explicitness_form)
+#         explicitness_end = explicitness_begin + len(explicitness_form)
 #
-# # excel의 id에 대한 json 파일 불러오기
+#         explicitness.__init__(explicitness_type, explicitness_form, explicitness_begin, explicitness_end)
+#         explicitness_list.append(explicitness.to_dict())
+#     # 여기까지 explicitness 생성
+#
+#     # 여기서부터
+#     k+=1
+#     expression_sentiment = temp_dict['output'][k] # 긍/부정 여부
+#     k+=1
+#     expression_intensity = temp_dict['output'][k] # 강도
+#     k+=1
+#     expression_domains = temp_dict['output'][k] # 영역
+#     k+=1
+#
+#     expression.__init__(explicitness_list, expression_sentiment, expression_domains, expression_intensity)
+#     immoral_expression.__init__(expression_id, expression_form, expression.to_dict())
+#     docu_data.immoral_expression.append(immoral_expression.to_dict())
+#     #여기까지 expression 생성
+#
+#
+# # json 데이터 불러오기
+# json_id = temp_dict['id'][0]
+# json_file_path = '/Users/dataly/Desktop/ABU/' + json_id.split('.')[0] + '.json'
+#
+# # # excel의 id에 대한 json 파일 불러오기
 # f = open(json_file_path)
 # data = json.load(f)
-# json_df = pd.json_normalize(data, record_path=['document'])
-
-
-
-
-# print(json_df)
-# print(excel_form)
-
-# 불러온 json DataFrame에서 excel의 id에 해당하는 부분 추출
+# search_result = find_object_by_id(data['document'], json_id)
+#
+# docu_data.id = search_result['id']
+# docu_data.metadata = search_result['metadata']
+# docu_data.paragraph = search_result['paragraph']
+#
+# json_data = json.dumps(docu_data.to_dict(), indent=4, ensure_ascii=False)
+# print(json_data)
+#
+#
+# # 초기화
+# temp_form = []
+# temp_form_id = []
+# temp_dict = {
+#     'id': [],
+#     'type': [],
+#     'output': [],
+#     'modify': [],
+#     'diff_1': [],
+#     'diff_2': []
+# }
 
 
